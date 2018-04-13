b8095dc6d0521907036273576bdec258039ca1b9
Pierre <pierre@hubert.fr>
2018-04-13 08:50:03 +0200

    Added connection to database

---
+++ application/config/application.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Application configuration
+ *
+ * @author Pierre HUBERT
+ */
+
+/**
+ * Application storage directory
+ *
+ * Warning ! With end trailing slash !
+ */
+$config["data_directory"] = APPPATH."data/";
--- application/config/database.php
+++ application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
+/*$db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => '',
@@ -93,4 +93,26 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
+);*/
+
+$db['default'] = array(
+    'dsn'      => '',
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+    'database' => './application/data/database.db',
+    'dbdriver' => 'sqlite3',
+    'dbprefix' => 'cr_',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt'  => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
