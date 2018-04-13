9254527993729cb1abf3f4c19d47f94559f6deec
Pierre <pierre@hubert.fr>
2018-04-13 08:26:40 +0200

    Ready to implement main page

---
+++ application/controllers/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Base controller for all the project
+ *
+ * @author Pierre HUBERT
+ */
+
+class BaseController extends CI_Controller {
+	
+}
---
+++ application/controllers/Home.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Home controller
+ *
+ * @author Pierre HUBERT
+ */
+
+require_once __DIR__."/BaseController.php";
+
+class Home extends BaseController {
+
+	/**
+	 * Main page of the project
+	 */
+	public function index(){
+
+		
+
+	}
+
+}
